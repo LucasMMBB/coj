8530a80dc399abd45a83838349d66f03cb15e87d
LucasMMBB <maoxummbb@gmail.com>
2018-02-11 11:19:54 -0500

    connect to mongodb with mongoose

--- w2/coj/coj-middleserver/server.js
+++ w2/coj/coj-middleserver/server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const restRouter = require('./routes/rest');
 
+const mongoose = require('mongoose');
+
+
+try {
+    mongoose.connect('mongodb://coj:coj@ds231568.mlab.com:31568/coj');
+} catch (error) {
+    console.log(error);
+}
+
 app.use('/api/v1', restRouter);
 
 app.get('/',(req,res)=>res.send("What's up?"));
